aeb8fa01e214816d973242c11fe7e272747a8608
cskiller24 <ccadayong2020@plm.edu.ph>
2022-06-27 01:17:15 +0800

    Added frontend vue

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <form method="post">
+
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+}
+</script>
